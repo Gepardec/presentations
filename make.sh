35598c2b323c60a4b1cdb513f819a0e04c9f826e
Thomas Herzog <thomas.herzog@gepardec.com>
2022-06-24 12:12:57 +0200

    Removed docker packages from dockerfile because not needed. Using locally built image with arm support

--- make.sh
+++ make.sh
@@ -33,11 +33,13 @@ for presentation in src/presentations/*; do
     echo "Begin: $(basename $presentation)"
     docker container rm -f presentation >/dev/null
     docker run -d -p 8080:8080 --entrypoint=/opt/revealjs/bin/present.py --name presentation presentations $(basename $presentation) 8080
+    sleep 0.5
     docker cp presentation:/opt/revealjs/index.html docs/$(basename $presentation).html
-    docker run --rm --net=host -t -v $(pwd):/slides astefanutti/decktape:3.1.0 reveal --size=1920x1080 --pause 2000 --load-pause 2000 http://localhost:8080 docs/$(basename $presentation).pdf
+    docker run --rm --net=host -t -v $(pwd):/slides cchet/decktape:3.1.0 reveal --size=1920x1080 --pause 2000 --load-pause 2000 http://localhost:8080 docs/$(basename $presentation).pdf
     echo "End: $(basename $presentation)"
 done
 
 docker container rm -f presentation >/dev/null
 docker run -d --entrypoint=/opt/revealjs/bin/present.py --name presentation presentations index 8080
+sleep 0.5
 docker cp presentation:/opt/revealjs/index.html docs/index.html
